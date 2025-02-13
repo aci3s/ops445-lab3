f5fef79b93222bc840a1ae815c5cc7c56034f9eb
Aashis Kharel <akharel2@myseneca.ca>
2025-02-13 18:38:29 -0500

    Additional files of lab3

---
+++ lab3b2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import lab3b
+
+
+print(lab3b.sum_numbers(10, 5))
+print(lab3b.sum_numbers(25, 25))
+print(lab3b.subtract_numbers(10, 5))
+print(lab3b.subtract_numbers(5, 10))
+print(lab3b.multiply_numbers(10, 5))
+print(lab3b.multiply_numbers(10, 2))
